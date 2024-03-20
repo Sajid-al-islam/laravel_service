ea532e63040a283606e208ca1b89c68852a02a75
Muhammad Sajidul Islam <sajid285.me@gmail.com>
2024-03-20 20:12:44 +0600

    optimizing

--- app/Http/Requests/UpdateUserRequest.php
+++ app/Http/Requests/UpdateUserRequest.php
@@ -27,7 +27,6 @@ class UpdateUserRequest extends FormRequest
             'name' => 'required',
             'phone_number' => 'required|string|max:18',
             'email' => [
-                'required',
                 'email',
                 Rule::unique('users')->ignore($this->authUser()->id),
             ],
